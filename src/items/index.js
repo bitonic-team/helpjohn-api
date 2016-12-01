bead00490d384af6313aff71d426fd2d4142e9a0
Antoine Chwalek <chwalek@intechinfo.fr>
2016-12-02 00:29:50 +0100

    Add item creation route

--- src/items/index.js
+++ src/items/index.js
@@ -1,8 +1,12 @@
 'use strict';
 const express = require('express');
-const pino = require('pino')({name:'Items'});
+const pino = require('pino')({
+    name:'Items',
+    level: 'trace'
+});
 
 const {db} = require('../db');
+const {authNeeded} = require('../users');
 const config = require('../config');
 
 module.exports = {};
@@ -12,16 +16,36 @@ const itemsRouter = module.exports.router = express.Router();
 itemsRouter.get('/', (req, res, next) => {
     let query = `SELECT * FROM items `;
 
-    if(req.query.zone){
-        query += ` WHERE zone = '${req.query.zone}'`;
-    }
+    const filters = Object.keys(req.query);
 
+    if(filters.length){
+        query += filters.reduce((acc, curr) => {
+            return `${acc} ${curr} = '${req.query.curr}'`;
+        }, ' WHERE ');
+    }
 
     return db.query(query, (err, users) => {
         if(err) {
             return next(err);
         }
         return res.json(users);
+    });
+});
+
+
+itemsRouter.post('/', authNeeded, (req, res, next) => {
+    pino.trace(`Item ${req.body.name} created by ${req.user.email}`, req.body);
+
+    const item = Object.assign({
+        zone: req.user.zone
+    }, req.body);
+    let query = `INSERT INTO items SET ?`;
+
+    return db.query(query, item,(err, item) => {
+        if(err) {
+            return next(err);
+        }
+        return res.status(201).json(item);
     });
 });
 
