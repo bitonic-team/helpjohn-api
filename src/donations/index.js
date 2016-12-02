85b2de3b7e04b80a95e1df5ec2e11e8691fa5e42
Antoine Chwalek <chwalek@intechinfo.fr>
2016-12-02 02:35:55 +0100

    Add donations

---
+++ src/donations/index.js
@@ -0,0 +1,58 @@
+'use strict';
+const express = require('express');
+const async = require('neo-async');
+const pino = require('pino')({
+    name:'Donations',
+    level: 'trace'
+});
+
+const {db} = require('../db');
+const config = require('../config');
+
+module.exports = {};
+
+const donationsRouter = module.exports.router = express.Router();
+
+donationsRouter.get('/', (req, res, next) => {
+
+    getDonations(req.query, (err, donations) =>{
+        if(err) {
+            return next(err);
+        }
+        return res.json(donations);
+    });
+});
+
+
+
+donationsRouter.post('/', (req, res, next) => {
+
+    const donation = req.body;
+    let query = `INSERT INTO donations SET ?`;
+
+    return db.query(query, donation,(err, result) => {
+        if(err) {
+            return next(err);
+        }
+        pino.debug(` ${donation.name} donate ${donation.amount} € for item ${donation.item}`, donation);
+
+        res.end();
+    });
+});
+
+
+
+function getDonations(filters, done){
+    let query = `SELECT * FROM donations `;
+
+    const filtersNames = Object.keys(filters);
+
+    if(filtersNames.length){
+        query += filtersNames.reduce((acc, curr) => {
+            return `${acc} ${curr} = '${filters[curr]}'`;
+        }, ' WHERE ');
+    }
+
+    pino.trace(`MySQL Get Donations : ${query}`);
+    return db.query(query, done)
+}
